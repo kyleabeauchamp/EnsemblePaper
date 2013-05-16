55fb6957a17e1316c537adba0b64bfeea0612686
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2013-05-15 23:11:47 -0700

    Multicross

--- code/calculations/multi_cross_val.py
+++ code/calculations/multi_cross_val.py
@@ -12,4 +12,5 @@ regularization_strength_list = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]
 ID = int(sys.argv[1])
 
 for k, (ff, prior, regularization_strength) in enumerate(itertools.product(ff_list, prior_list, regularization_strength_list)):
-    print(k, ff, prior, regularization_strength)
+    if k == ID:
+        print(k, ff, prior, regularization_strength)
