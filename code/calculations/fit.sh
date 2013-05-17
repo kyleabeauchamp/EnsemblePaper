80e9a5cd76fa50087f4fb1c1b3f40a50d215bb00
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2013-05-17 15:03:18 -0700

    PlotMCMCTrace, plotCrossVal

--- code/calculations/fit.sh
+++ code/calculations/fit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cmd="python /home/kyleb/src/kyleabeauchamp/EnsemblePaper/code/calculations/ALA3_fit.py amber96 maxent "
-$cmd >& /home/kyleb/cross_val-${SGE_JOB_ID}-${SGE_TASK_ID}.log
+$cmd >& /home/kyleb/fit-${SGE_JOB_ID}.log
