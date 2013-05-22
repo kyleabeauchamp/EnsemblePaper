cccb26de390e65ecd8f53c6154b69fe45796d7f9
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2013-05-22 11:50:47 -0700

    Check in a bunch of code, half working.  Towards using Pandas for *everything*.

--- code/calculations/fit.sh
+++ code/calculations/fit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cmd="python /home/kyleb/src/kyleabeauchamp/EnsemblePaper/code/calculations/ALA3_fit.py amber96 maxent "
-$cmd >& /home/kyleb/fit-${SGE_JOB_ID}.log
+cmd="python /home/kyleb/src/kyleabeauchamp/EnsemblePaper/code/calculations/fit_model.py amber96 maxent "
+$cmd >& /home/kyleb/fit-${JOB_ID}.log
