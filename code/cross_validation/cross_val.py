45ff61a10ddb04720c8ec5518a47836548f466cc
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2013-06-25 22:39:01 -0700

    Cross Val

--- code/cross_validation/cross_val.py
+++ code/cross_validation/cross_val.py
@@ -1,21 +1,21 @@
 import experiment_loader
 import ALA3
 import numpy as np
-from fitensemble import lvbp
+from fitensemble import belt
 import sys
 
-lvbp.ne.set_num_threads(1)
+belt.ne.set_num_threads(1)
 
 def run(ff, prior, regularization_strength):
     predictions, measurements, uncertainties = experiment_loader.load(ff, stride=ALA3.cross_val_stride)
     if prior == "maxent":
-        model_factory = lambda predictions, measurements, uncertainties: lvbp.MaxEnt_LVBP(predictions, measurements, uncertainties, regularization_strength)
+        model_factory = lambda predictions, measurements, uncertainties: belt.MaxEnt_BELT(predictions, measurements, uncertainties, regularization_strength)
     else:
         precision = np.cov(predictions.values.T)
-        model_factory = lambda predictions, measurements, uncertainties: lvbp.MVN_LVBP(predictions, measurements, uncertainties, regularization_strength, precision=precision)
+        model_factory = lambda predictions, measurements, uncertainties: belt.MVN_BELT(predictions, measurements, uncertainties, regularization_strength, precision=precision)
 
     bootstrap_index_list = np.array_split(np.arange(len(predictions)), ALA3.kfold)
-    train_chi, test_chi = lvbp.cross_validated_mcmc(predictions.values, measurements.values, uncertainties.values, model_factory, bootstrap_index_list, ALA3.num_samples, thin=ALA3.thin)
+    train_chi, test_chi = belt.cross_validated_mcmc(predictions.values, measurements.values, uncertainties.values, model_factory, bootstrap_index_list, ALA3.num_samples, thin=ALA3.thin)
 
     print regularization_strength, train_chi.mean(), test_chi.mean()
     F = open(ALA3.cross_val_filename, 'a')
